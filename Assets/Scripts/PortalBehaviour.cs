7dfbc0a9fe106923886391a68c3ca15285c02acf
Barak-Kuzi <barakkuzi1997@gmail.com>
2025-05-16 23:37:59 +0300

    Adding a second scene

---
+++ Assets/Scripts/PortalBehaviour.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalBehaviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+    }
+}
